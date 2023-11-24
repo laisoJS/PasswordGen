eb15f1e3d9812f1b04e1675a22e8ef6cd9a0b2da
laisoJS <aloisleroy2016@gmail.com>
2023-11-24 15:15:45 +0100

    patching errors

--- passwordGenCLI.py
+++ passwordGenCLI.py
@@ -10,7 +10,29 @@ ERROR_MESSAGE = {
     "FileCreationError": "ERROR: Unable to create the file",
 }
 
+
 def generate_password(length, include_uppercase, include_lowercase, include_numbers, include_symbols):
+    """
+    Generate a random password based on specified criteria.
+
+    :param length: Length of the password.
+    :type length: int
+
+    :param include_uppercase: Include uppercase letters.
+    :type include_uppercase: bool
+
+    :param include_lowercase: Include lowercase letters.
+    :type include_lowercase: bool
+
+    :param include_numbers: Include numbers.
+    :type include_numbers: bool
+
+    :param include_symbols: Include symbols.
+    :type include_symbols: bool
+
+    :return: A randomly generated password.
+    :rtype: str
+    """
     characters = ''.join([
         string.ascii_uppercase if include_uppercase else '',
         string.ascii_lowercase if include_lowercase else '',
@@ -22,6 +44,18 @@ def generate_password(length, include_uppercase, include_lowercase, include_numb
 
 
 def main():
+    """
+    Main function to generate passwords based on provided arguments.
+
+    Handles argument parsing, password generation, and optional file zipping.
+
+    Raises:
+        ValueError: If the provided length is smaller than 8.
+        IOError: If there's an issue creating the file.
+
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(description='Password Generator')
     parser.add_argument('-l', '--length', type=int, default=8, help='Length of the password (default: 8)')
     parser.add_argument('-u', '--upper', action='store_true', help='Include uppercase letters')
@@ -30,8 +64,8 @@ def main():
     parser.add_argument('-s', '--symbols', action='store_true', help='Include symbols')
     parser.add_argument('-q', '--quantity', type=int, default=1, help='Number of passwords to generate (default: 1)')
     parser.add_argument('-o', '--output', default='passwords.txt', help='Output file name')
-    parser.add_argument('-z', '--zip', action='store_true', help='Create a .tar.gz file')
     parser.add_argument('--sep', type=int, default=1, help='Number of files to create')
+    parser.add_argument('-z', '--zip', action='store_true', help='Create a .tar.gz file')
 
     args = parser.parse_args()
 
@@ -54,20 +88,28 @@ def main():
 
         passwords_per_file = (num_passwords + num_files - 1) // num_files
 
-        for i in range(num_files):
-            start_idx = i * passwords_per_file
-            end_idx = min((i + 1) * passwords_per_file, num_passwords)
-            file_passwords = passwords[start_idx:end_idx]
-
-            file_name = f"{filename}_{i + 1}.txt"
-            with open(file_name, 'w') as file:
-                file.writelines('%s\n' % password for password in file_passwords)
-
         if args.zip:
+            for i in range(num_files):
+                file_name = f"{filename}_{i + 1}.txt"
+                with open(file_name, 'w') as file:
+                    file_passwords = passwords[i * passwords_per_file:(i + 1) * passwords_per_file]
+                    file.writelines('%s\n' % password for password in file_passwords)
+
             with tarfile.open(f'{args.output}.tar.gz', 'w:gz') as tar:
                 for i in range(num_files):
                     file_name = f"{filename}_{i + 1}.txt"
                     tar.add(file_name, arcname=os.path.basename(file_name))
+
+            # Clean up: remove individual text files after zipping
+            for i in range(num_files):
+                file_name = f"{filename}_{i + 1}.txt"
+                os.remove(file_name)
+        else:
+            for i in range(num_files):
+                file_name = f"{filename}_{i + 1}.txt"
+                with open(file_name, 'w') as file:
+                    file_passwords = passwords[i * passwords_per_file:(i + 1) * passwords_per_file]
+                    file.writelines('%s\n' % password for password in file_passwords)
 
     except ValueError:
         print(ERROR_MESSAGE["TooSmall"])
@@ -77,4 +119,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
