0ac1b82c66d574ff84fab34506fbe0b19baebfa3
laisoJS <aloisleroy2016@gmail.com>
2023-11-29 11:57:45 +0100

    Patching output filename error

--- passwordGenCLI.py
+++ passwordGenCLI.py
@@ -1,8 +1,8 @@
+import argparse
+import os
 import random
 import string
-import argparse
 import tarfile
-import os
 
 ERROR_MESSAGE = {
     "TooSmall": "ERROR: The minimum password length is 8 characters",
@@ -12,27 +12,6 @@ ERROR_MESSAGE = {
 
 
 def generate_password(length, include_uppercase, include_lowercase, include_numbers, include_symbols):
-    """
-    Generate a random password based on specified criteria.
-
-    :param length: Length of the password.
-    :type length: int
-
-    :param include_uppercase: Include uppercase letters.
-    :type include_uppercase: bool
-
-    :param include_lowercase: Include lowercase letters.
-    :type include_lowercase: bool
-
-    :param include_numbers: Include numbers.
-    :type include_numbers: bool
-
-    :param include_symbols: Include symbols.
-    :type include_symbols: bool
-
-    :return: A randomly generated password.
-    :rtype: str
-    """
     characters = ''.join([
         string.ascii_uppercase if include_uppercase else '',
         string.ascii_lowercase if include_lowercase else '',
@@ -44,18 +23,6 @@ def generate_password(length, include_uppercase, include_lowercase, include_numb
 
 
 def main():
-    """
-    Main function to generate passwords based on provided arguments.
-
-    Handles argument parsing, password generation, and optional file zipping.
-
-    Raises:
-        ValueError: If the provided length is smaller than 8.
-        IOError: If there's an issue creating the file.
-
-    Returns:
-        None
-    """
     parser = argparse.ArgumentParser(description='Password Generator')
     parser.add_argument('-l', '--length', type=int, default=8, help='Length of the password (default: 8)')
     parser.add_argument('-u', '--upper', action='store_true', help='Include uppercase letters')
@@ -63,7 +30,7 @@ def main():
     parser.add_argument('-n', '--numbers', action='store_true', help='Include numbers')
     parser.add_argument('-s', '--symbols', action='store_true', help='Include symbols')
     parser.add_argument('-q', '--quantity', type=int, default=1, help='Number of passwords to generate (default: 1)')
-    parser.add_argument('-o', '--output', default='passwords.txt', help='Output file name')
+    parser.add_argument('-o', '--output', default='', help='Output file name')
     parser.add_argument('--sep', type=int, default=1, help='Number of files to create')
     parser.add_argument('-z', '--zip', action='store_true', help='Create a .tar.gz file')
 
@@ -79,7 +46,10 @@ def main():
         include_numbers = args.numbers or (not any([args.upper, args.lower, args.numbers, args.symbols]))
         include_symbols = args.symbols or (not any([args.upper, args.lower, args.numbers, args.symbols]))
 
-        filename = args.output
+        if args.output:  # If the output argument is provided
+            filename = args.output if not args.output.endswith('.txt') else args.output[:-4]
+        else:
+            filename = 'password'
 
         passwords = [
             generate_password(length, include_uppercase, include_lowercase, include_numbers, include_symbols)
@@ -90,23 +60,31 @@ def main():
 
         if args.zip:
             for i in range(num_files):
-                file_name = f"{filename}_{i + 1}.txt"
+                file_suffix = f"_{i + 1}" if num_files > 1 else ''
+                file_name = f"{filename}{file_suffix}.txt" if args.sep else f"{filename}.txt"
+
                 with open(file_name, 'w') as file:
                     file_passwords = passwords[i * passwords_per_file:(i + 1) * passwords_per_file]
                     file.writelines('%s\n' % password for password in file_passwords)
 
-            with tarfile.open(f'{args.output}.tar.gz', 'w:gz') as tar:
+            with tarfile.open(f'{filename}.tar.gz', 'w:gz') as tar:
                 for i in range(num_files):
-                    file_name = f"{filename}_{i + 1}.txt"
+                    file_suffix = f"_{i + 1}" if num_files > 1 else ''
+                    file_name = f"{filename}{file_suffix}.txt" if args.sep else f"{filename}.txt"
+
                     tar.add(file_name, arcname=os.path.basename(file_name))
 
             # Clean up: remove individual text files after zipping
             for i in range(num_files):
-                file_name = f"{filename}_{i + 1}.txt"
+                file_suffix = f"_{i + 1}" if num_files > 1 else ''
+                file_name = f"{filename}{file_suffix}.txt" if args.sep else f"{filename}.txt"
+
                 os.remove(file_name)
         else:
             for i in range(num_files):
-                file_name = f"{filename}_{i + 1}.txt"
+                file_suffix = f"_{i + 1}" if num_files > 1 else ''
+                file_name = f"{filename}{file_suffix}.txt" if args.sep else f"{filename}.txt"
+
                 with open(file_name, 'w') as file:
                     file_passwords = passwords[i * passwords_per_file:(i + 1) * passwords_per_file]
                     file.writelines('%s\n' % password for password in file_passwords)
